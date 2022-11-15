e487c3f0c6180de1ec80aa014687f894871d176e
Your Name <email@example.com>
2022-11-15 15:29:08 +0800

    Initial commit

---
+++ src/main/java/com/example/springbootstudy/config/MainConfiguration.java
@@ -0,0 +1,11 @@
+package com.example.springbootstudy.config;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 刘上忠
+ * @data studying
+ */
+@Configuration
+public class MainConfiguration {
+}
